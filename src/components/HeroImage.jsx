2d8276c38e1b07a35285fe596c85b49b04c2eada
Amna1993 <amnarazzaqawan@gmail.com>
2024-11-26 10:41:11 -0500

    alt fixed

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -5,7 +5,7 @@ const HeroImage = () => {
     <div className="lg:w-1/2 mt-10 lg:mt-0">
       <img
         src="https://trees.org/wp-content/uploads/2022/10/0N7A8255-PILI-scaled-e1707327214438.jpg"
-        alt="Planting trees"
+        alt=""
         className="rounded-lg shadow-lg"
       />
     </div>
--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -6,7 +6,7 @@ const ImageContainer = () => {
       <div className="lg:absolute w-full lg:w-screen-65 h-82 lg:h-full z-0 right-0 top-0 overflow-hidden rounded-bl-80 lg:rounded-bl-100">
         <img
           src="https://trees.org/wp-content/uploads/2022/10/0N7A8255-PILI-scaled-e1707327214438.jpg"
-          alt="Trees for the Future encourages agroforestry"
+          alt=""
           className="w-full h-full object-center object-cover"
         />
       </div>
