30fd5e96d4ab6cf4f4207d9929aa1d4fdfc02b41
Amna1993 <amnarazzaqawan@gmail.com>
2024-11-26 10:58:35 -0500

    Navbar fixed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import './App.css';
 import About from './components/About/About';
 import ProblemCauseSolution from './components/About/ProblemCauseSolution';
 import HeroSection from './components/Hero/HeroSection';
-import Navbar from './components/Navbar/Navbar';
 import StatsSection from './components/StatsSection';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
       <HeroSection />
       <About />
       <ProblemCauseSolution />
